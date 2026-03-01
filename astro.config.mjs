6a5d0e05c505ae4a349a888db38a46ab76f4f9d6
Wanda <wanda.devops@gmail.com>
2026-03-01 17:24:17 +0100

    fix: deploy to root github.io repo (no subpath issues)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://wanda-os-dev.github.io',
-  base: '/wandasystems-site',
   integrations: [
     react(),
     tailwind({
